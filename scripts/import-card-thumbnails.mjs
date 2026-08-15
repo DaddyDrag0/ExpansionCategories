3882ba7d7907777818ae4b0c26064f30a05000ff
Drago <dragoidc3@gmail.com>
2026-08-15 00:26:50 -0400

    Remove temporary thumbnail import script

--- scripts/import-card-thumbnails.mjs
+++
@@ -1,8 +0,0 @@
-import fs from 'node:fs'
-
-const url='https://raw.githubusercontent.com/DaddyDrag0/CardRngExpansionDepths/main/src/data/thumbnails.json'
-const response=await fetch(url,{headers:{'user-agent':'ExpansionCategories'}})
-if(!response.ok)throw new Error(`Thumbnail source failed: ${response.status}`)
-const thumbs=await response.json()
-fs.writeFileSync('thumbnails.json',JSON.stringify(thumbs,null,2)+'\n')
-console.log(`Imported ${Object.keys(thumbs).length} card/aura thumbnails.`)
